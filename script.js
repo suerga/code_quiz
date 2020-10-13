11bd4c1cd52f8e703b36f0e87f50ba65eda8fbe0
suerga <anyitian808@gmail.com>
2020-10-12 20:27:47 -0400

    basic

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// link all variables
+
 var startBtn = document.querySelector("#start-btn");
 var nextBtn = document.querySelector("#next-btn");
 var start = document.querySelector(".start");
@@ -5,30 +7,61 @@ var questionContainer = document.querySelector("#question-container");
 var questionElement = document.querySelector("#question");
 var answerBtn = document.querySelector("#answer-btn");
 
-let shuffledQuestions, currentQuestionIndex;
-
-startBtn.addEventListener("click", startGame);
-nextButton.addEventListener("click", () => {
-  currentQuestionIndex++;
-  setNextQuestion();
-});
-
-function startGame() {
-  console.log("started");
-  start.classList.add("hide");
-  questionContainer.classList.remove("hide");
-  nextBtn.classList.remove("hide");
-  currentQuestionIndex = 0;
-  setNextQuestion();
-}
-
-function setNextQuestion() {}
-
-function showQuestion() {}
-
-function selectAnswer() {}
-
-const questions = [
+var questions = [
+  {
+    question: "Q1: Is web development fun?",
+    answers: [
+      { text: "Kinda", correct: false },
+      { text: "YES!!!", correct: true },
+      { text: "Um no", correct: false },
+      { text: "IDK", correct: false },
+    ],
+  },
+  {
+    question: "Q2:Is web development fun?",
+    answers: [
+      { text: "Kinda", correct: false },
+      { text: "YES!!!", correct: true },
+      { text: "Um no", correct: false },
+      { text: "IDK", correct: false },
+    ],
+  },
+  {
+    question: "Is web development fun?",
+    answers: [
+      { text: "Kinda", correct: false },
+      { text: "YES!!!", correct: true },
+      { text: "Um no", correct: false },
+      { text: "IDK", correct: false },
+    ],
+  },
+  {
+    question: "Is web development fun?",
+    answers: [
+      { text: "Kinda", correct: false },
+      { text: "YES!!!", correct: true },
+      { text: "Um no", correct: false },
+      { text: "IDK", correct: false },
+    ],
+  },
+  {
+    question: "Is web development fun?",
+    answers: [
+      { text: "Kinda", correct: false },
+      { text: "YES!!!", correct: true },
+      { text: "Um no", correct: false },
+      { text: "IDK", correct: false },
+    ],
+  },
+  {
+    question: "Is web development fun?",
+    answers: [
+      { text: "Kinda", correct: false },
+      { text: "YES!!!", correct: true },
+      { text: "Um no", correct: false },
+      { text: "IDK", correct: false },
+    ],
+  },
   {
     question: "Is web development fun?",
     answers: [
@@ -39,3 +72,64 @@ const questions = [
     ],
   },
 ];
+
+var currentQuestionIndex = 0;
+
+startBtn.addEventListener("click", startGame);
+nextBtn.addEventListener("click", addNumber);
+
+function addNumber() {
+  currentQuestionIndex++;
+}
+
+function startGame() {
+  console.log("started");
+  start.classList.add("hide");
+  questionContainer.classList.remove("hide");
+  nextBtn.classList.remove("hide");
+}
+
+function showQuestion() {
+  questionElement.textContent = questions[currentQuestionIndex].question;
+}
+
+showQuestion();
+
+function showAnswer() {
+  questions[currentQuestionIndex].answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.textContent = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+      button.addEventListener("click", selectAnswer);
+    }
+    answerBtn.appendChild(button);
+  });
+}
+showAnswer();
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerBtn.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
+
+console.log(currentQuestionIndex);
